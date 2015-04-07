edd316490c771df231838af7a969ae90fd8552b8
witwave <linuxwit@gmail.com>
2015-04-07 17:13:15 +0800

    add password

--- libs/routes/users.js
+++ libs/routes/users.js
@@ -6,18 +6,44 @@ var libs = process.cwd() + '/libs/';
 
 var db = require(libs + 'db/mongoose');
 
-router.get('/info', passport.authenticate('bearer', { session: false }),
+router.post('/info',
+    passport.authenticate('bearer', {
+        session: false
+    }),
     function(req, res) {
-        // req.authInfo is set using the `info` argument supplied by
-        // `BearerStrategy`.  It is typically used to indicate scope of the token,
-        // and used in access control checks.  For illustrative purposes, this
-        // example simply returns the scope in the response.
-        res.json({ 
-        	user_id: req.user.userId, 
-        	name: req.user.username, 
-        	scope: req.authInfo.scope 
+        res.json({
+            user_id: req.user.userId,
+            name: req.user.username,
+            scope: req.authInfo.scope
         });
     }
 );
+
+router.post('/setpwd',
+    passport.authenticate('bearer', {
+        session: false
+    }),
+    function(req, res) {
+        res.json({
+            status: true,
+            user_id: req.user.userId,
+            name: req.user.username
+        });
+    }
+);
+
+
+router.post('/resetpwd', function(req, res) {
+    res.json({
+        status: true
+    });
+});
+
+router.post('/forgetpwd', function(req, res) {
+    res.json({
+        status: true
+    });
+});
+
 
 module.exports = router;
